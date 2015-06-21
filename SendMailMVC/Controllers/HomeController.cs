658a6b1b17dafea94ef9338ae2857894ab13fd40
vanloc0301 <locha9066@gmail.com>
2015-06-22 01:14:35 +0700

    send mail from contact, chang class contact in homecontroller

--- SendMailMVC/Controllers/HomeController.cs
+++ SendMailMVC/Controllers/HomeController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SendMailMVC.Models;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
 
 namespace SendMailMVC.Controllers
 {
@@ -20,11 +24,36 @@ namespace SendMailMVC.Controllers
             return View();
         }
 
-        public ActionResult Contact()
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Contact(EmailFormModel model)
         {
-            ViewBag.Message = "Your contact page.";
+            if (ModelState.IsValid)
+            {
+                var body = "<p>Email từ: {0} ({1})</p><p> Tin nhắn: </p><p>{2}</p>";
+                var message = new MailMessage();
+                message.To.Add(new MailAddress("vanloc0301@gmail.com"));
+                message.From = new MailAddress("vanloc0301@outlook.com");
+                message.Subject = "Your email subject";
+                message.Body = string.Format(body, model.FromName, model.FromEmail, model.Message);
+                message.IsBodyHtml = true;
 
-            return View();
+                using (var smtp = new SmtpClient())
+                {
+                    var credential = new NetworkCredential
+                    {
+                        UserName = "vanloc0301@outlook.com",
+                        Password = "r0ysy0301"
+                    };
+                    smtp.Credentials = credential;
+                    smtp.Host = "smtp-outlook.com";
+                    smtp.Port = 587;
+                    smtp.EnableSsl = true;
+                    await smtp.SendMailAsync(message);
+                    return RedirectToAction("Sent");
+                }
+            }
+            return View(model);
         }
     }
 }
---
+++ SendMailMVC/Models/EmailFormModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SendMailMVC.Models
+{
+    public class EmailFormModel
+    {
+        [Required, Display(Name="Họ Tên:")]
+        public string FromName { get; set; }
+
+        [Required, Display(Name="Email của bạn"), EmailAddress]
+        public string FromEmail { get; set; }
+
+        [Required]
+        public string Message { get; set; }
+    }
+}
